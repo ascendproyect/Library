0611782415df06438b1b14b91a7256775a262b15
skid.leandro() <98243196+LxandroSSJ@users.noreply.github.com>
2023-11-17 16:00:20 -0300

    Update Config

--- src/main/java/dev/hely/lib/configuration/Config.java
+++ src/main/java/dev/hely/lib/configuration/Config.java
@@ -5,6 +5,7 @@ package dev.hely.lib.configuration;
  * domingo, marzo 28, 2021
  */
 
+import dev.hely.lib.CC;
 import lombok.Getter;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -12,6 +13,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Getter
 public class Config {
@@ -24,6 +28,7 @@ public class Config {
         if (!file.exists()) {
             plugin.saveResource(name + ".yml", false);
         }
+
         this.configuration = YamlConfiguration.loadConfiguration(this.getFile());
     }
 
@@ -39,13 +44,41 @@ public class Config {
         this.configuration = YamlConfiguration.loadConfiguration(this.getFile());
     }
 
+    public double getDouble(String path) {
+        if (this.configuration.contains(path)) return this.configuration.getDouble(path);
+        return 0.0D;
+    }
+
+    public int getInt(String path) {
+        if (this.configuration.contains(path)) return this.configuration.getInt(path);
+        return 0;
+    }
+
+    public boolean getBoolean(String path) {
+        if (this.configuration.contains(path)) return this.configuration.getBoolean(path);
+        return true;
+    }
+
+    public String getString(String path) {
+        if (this.configuration.contains(path)) return this.configuration.getString(CC.translate(path));
+        return CC.translate("&cString not found: '%path%'").replace("%path%", path);
+    }
+
+    public List<String> getStringList(String path) {
+        if (this.configuration.contains(path)) {
+            ArrayList<String> arrayList = new ArrayList<>();
+            for (String string : this.configuration.getStringList(path))
+                arrayList.add(CC.translate(string));
+            return arrayList;
+        }
+        return Collections.singletonList(CC.translate("&cString not found: '%path%'").replace("%path%", path));
+    }
+
     public File getFile() {
         return this.file;
     }
 
-
     public YamlConfiguration getConfig() {
         return this.configuration;
     }
-
 }
